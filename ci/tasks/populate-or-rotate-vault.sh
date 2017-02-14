3d514d29da62d05f7956fe8d9d4ddb0fc72f7a51
A.S. Lomoff <lomov.as@gmail.com>
2017-02-13 22:18:58 -0800

    write uaa ldap user password to vault after populate or rotate vault

--- ci/tasks/populate-or-rotate-vault.sh
+++ ci/tasks/populate-or-rotate-vault.sh
@@ -37,4 +37,7 @@ omg-cli/omg-linux deploy-product \
   --vault-rotate \
   --vault-token $VAULT_TOKEN > throw-away-manifest.yml
 
+vault read --format=json $VAULT_HASH_PASSWORD | jq .data > passwords.json
+vault write secret/cf-wdc1-prod-password @passwords.json --var="uaa-ldap-user-password=$UAA_LDAP_PASSWORD"
+
 #eof

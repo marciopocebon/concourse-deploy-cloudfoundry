be720f49b0b54275b0e657311815f60f128b9b50
s-matyukevich <s.matyukevich@gmail.com>
2017-04-12 12:08:57 -0700

    rabbitmq update

--- ci/tasks/update-pipelines.sh
+++ ci/tasks/update-pipelines.sh
@@ -24,28 +24,6 @@ update_pipeline bluemedora $DEPLOY_BLUEMEDORA_GIT_URL
 update_pipeline firehose-to-loginsight $DEPLOY_FIREHOSE_TO_LOGINSIGHT_GIT_URL
 update_pipeline spring-services $DEPLOY_SPRING_SERVICES_GIT_URL
 
-bosh_client_id=$(vault read -field=bosh-client-id secret/bosh-$FOUNDATION_NAME-props)
-bosh_client_secret=$(vault read -field=bosh-client-secret secret/bosh-$FOUNDATION_NAME-props)
-bosh_cacert=$(vault read -field=bosh-cacert secret/bosh-$FOUNDATION_NAME-props)
-
-export CONCOURSE_URI=$CONCOURSE_URL
-export CONCOURSE_TARGET=$FOUNDATION_NAME
-export PIPELINE_REPO_BRANCH=master
-echo "$GIT_PRIVATE_KEY" > git-private-key.pem
-export PIPELINE_REPO_PRIVATE_KEY_PATH=../git-private-key.pem
-export BOSH_ENVIRONMENT=${BOSH_URL#https://}
-export BOSH_CLIENT=$bosh_client_id
-export BOSH_CLIENT_SECRET=$bosh_client_secret
-echo "$bosh_cacert" > bosh-ca-cert.pem
-export BOSH_CA_CERT=../bosh-ca-cert.pem
-
-pushd concourse-deploy-rabbitmq
-export PRODUCT_NAME=rabbitmq
-export PIPELINE_REPO=$DEPLOY_RABBITMQ_GIT_URL
-./setup-pipeline.sh
-popd
-
-
 all_ips=$(prips $(echo "$PCF_SERVICES_STATIC" | sed 's/-/ /'))
 OLD_IFS=$IFS
 IFS=$'\n'
@@ -65,6 +43,20 @@ get_ips(){
   echo "$res" | cut -c 2-
 }
 
+bosh_client_id=$(vault read -field=bosh-client-id secret/bosh-$FOUNDATION_NAME-props)
+bosh_client_secret=$(vault read -field=bosh-client-secret secret/bosh-$FOUNDATION_NAME-props)
+bosh_cacert=$(vault read -field=bosh-cacert secret/bosh-$FOUNDATION_NAME-props)
+
+export CONCOURSE_URI=$CONCOURSE_URL
+export CONCOURSE_TARGET=$FOUNDATION_NAME
+export PIPELINE_REPO_BRANCH=master
+echo "$GIT_PRIVATE_KEY" > git-private-key.pem
+export PIPELINE_REPO_PRIVATE_KEY_PATH=../git-private-key.pem
+export BOSH_ENVIRONMENT=${BOSH_URL#https://}
+export BOSH_CLIENT=$bosh_client_id
+export BOSH_CLIENT_SECRET=$bosh_client_secret
+echo "$bosh_cacert" > bosh-ca-cert.pem
+export BOSH_CA_CERT=../bosh-ca-cert.pem
 
 pushd concourse-deploy-p-mysql
 export PRODUCT_NAME=p-mysql
@@ -114,3 +106,20 @@ cat > concourse-deploy-turbulence/deployment-props.json <<EOF
 EOF
 vault write secret/turbulence-$FOUNDATION_NAME-props @concourse-deploy-turbulence/deployment-props.json
 update_pipeline turbulence $DEPLOY_TURBULENCE_GIT_URL
+
+pushd concourse-deploy-rabbitmq
+export PRODUCT_NAME=rabbitmq
+export PIPELINE_REPO=$DEPLOY_RABBITMQ_GIT_URL
+cat > deployment-props.json <<EOF
+{
+  "network": "pcf-services",
+  "pivnet_api_token": "$PIVNET_API_TOKEN",
+  "rabbit-public-ip": "$(get_ips 1)",
+  "rabbit-server-ip": "$(get_ips 1)",
+  "rabbit-broker-ip": "$(get_ips 1)",
+  "syslog-address": ""
+}
+EOF
+./setup-pipeline.sh
+popd
+
